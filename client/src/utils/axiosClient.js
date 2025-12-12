7259ca8479a6645bd545f90694fe63a7185422d4
Rana Dutta <duttarana189@gmail.com>
2025-12-12 19:09:34 +0530

    fix: remove fallback URL for axiosClient baseURL

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.BACKEND_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.BACKEND_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
